2221743b3683e42d397e90249d1cf19ae8011f8d
Антон Лучкин <luchkin1988@gmail.com>
2020-09-25 00:08:43 +0300

    Updates

---
+++ precache-manifest.5c0019445594e47860a204af229d3592.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70340a87f571fe7fcde8783145ac3386",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "cc7fa79ffeaa8d25ce5a",
+    "url": "/goit-react-hw-04-movies/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "01a14edad7e501169f3b",
+    "url": "/goit-react-hw-04-movies/static/css/main.996cbe6a.chunk.css"
+  },
+  {
+    "revision": "cc7fa79ffeaa8d25ce5a",
+    "url": "/goit-react-hw-04-movies/static/js/2.1a677f63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.1a677f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a14edad7e501169f3b",
+    "url": "/goit-react-hw-04-movies/static/js/main.630c5c92.chunk.js"
+  },
+  {
+    "revision": "952c08333dad3729b462",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.ad4bcf9c.js"
+  }
+]);
